e84ed71f9467b594b6807c07cea3f911760544f7
Axelo <133879412+AxeloLabs@users.noreply.github.com>
2025-10-12 20:34:50 -0400

    chore: dc

--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -1,4 +1,8 @@
-export * from './config'
-export * from './auth'
-export * from './dataconnect'
-export * from './hooks'
+export * from "./config";
+export * from "./auth";
+export * from "./dataconnect";
+
+// Exporter tout le SDK généré
+export * from "./generated";
+
+export * from "./hooks";
